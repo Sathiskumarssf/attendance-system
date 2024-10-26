c9624dbe1870eb8a5aca712db475edf622c0cc19
Educative Learner <sathiskumarsk2001@gmail.com>
2024-10-26 14:04:09 +0530

    #3 initial setup of backend in springbook

---
+++ server/src/main/java/com/attendance_system/sever/SeverApplication.java
@@ -0,0 +1,22 @@
+package com.attendance_system.sever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+
+@SpringBootApplication
+public class SeverApplication implements ApplicationListener<WebServerInitializedEvent> {
+
+	private int serverPort;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeverApplication.class, args);
+	}
+
+	@Override
+	public void onApplicationEvent(WebServerInitializedEvent event) {
+		this.serverPort = event.getWebServer().getPort();
+		System.out.println("Backend is running on port: " + this.serverPort);
+	}
+}
---
+++ server/src/test/java/com/attendance_system/sever/SeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendance_system.sever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
